dbeeec175e90229f8d249a46b85004e5abe0c06a
roberrera <roberterrera@gmail.com>
2016-03-03 00:22:32 -0500

    Add button methods; update xml

---
+++ SyncAdaptersLab/app/src/main/java/com/roberterrera/syncadapterslab/StockItem.java
@@ -0,0 +1,26 @@
+package com.roberterrera.syncadapterslab;
+
+/**
+ * Created by Rob on 3/2/16.
+ */
+public class StockItem {
+
+    String name;
+    String lastPrice;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastPrice() {
+        return lastPrice;
+    }
+
+    public void setLastPrice(String lastPrice) {
+        this.lastPrice = lastPrice;
+    }
+}
